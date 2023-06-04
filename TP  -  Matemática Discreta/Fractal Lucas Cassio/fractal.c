49be657ac7f0b22535bee4164d771fcd998b21bf
lucascassio <lccosta2604@gmail.com>
2023-06-04 16:40:22 -0300

    dkakdka

--- TP  -  Matemática Discreta/Fractal Lucas Cassio/fractal.c
+++ TP  -  Matemática Discreta/Fractal Lucas Cassio/fractal.c
@@ -1,63 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <SDL2/SDL.h>
+#include <string.h>
 
 #define MAX_STRING_LENGTH 1000000
 
 int main()
 {
-  char axiom[] = "X";
-  char ruleX[] = "YF+XF+Y";
-  char ruleY[] = "XF-YF-X";
+    char axiom[] = "X";
+    char ruleX[] = "YF+XF+Y";
+    char ruleY[] = "XF-YF-X";
 
+    char current_string[MAX_STRING_LENGTH] = {0};
+    char next_string[MAX_STRING_LENGTH] = {0};
+    int i, j, k, num_iterations;
 
-  char current_string[MAX_STRING_LENGTH] = {0};
-  char next_string[MAX_STRING_LENGTH] = {0};
-  int i, j, k, num_iterations;
+    // Set the current string to the axiom
+    strcpy(current_string, axiom);
 
-  // Set the current string to the axiom
-  strcpy(current_string, axiom);
+    // Iterate the specified number of times
+    for (num_iterations = 0; num_iterations < 4; num_iterations++) {
+        // Replace each 'X' in the current string with the X rule
+        for (i = 0, j = 0; current_string[i] != '\0'; i++) {
+            if (current_string[i] == 'X') {
+                for (k = 0; ruleX[k] != '\0'; k++) {
+                    next_string[j++] = ruleX[k];
+                }
+            }
+            // Replace each 'Y' in the current string with the Y rule
+            else if (current_string[i] == 'Y') {
+                for (k = 0; ruleY[k] != '\0'; k++) {
+                    next_string[j++] = ruleY[k];
+                }
+            } else {
+                next_string[j++] = current_string[i];
+            }
+        }
+        next_string[j] = '\0';
 
-  // Iterate the specified number of times
-  for (num_iterations = 0; num_iterations < 4; num_iterations++) {
-    // Replace each 'X' in the current string with the X rule
+        // Copy the next string to the current string
+        strcpy(current_string, next_string);
+    }
+
+    // Remove all occurrences of X and Y from the final string
     for (i = 0, j = 0; current_string[i] != '\0'; i++) {
-      if (current_string[i] == 'X') {
-        for (k = 0; ruleX[k] != '\0'; k++) {
-          next_string[j++] = ruleX[k];
+        if (current_string[i] == 'X' || current_string[i] == 'Y') {
+            continue;
+        } else {
+            current_string[j++] = current_string[i];
         }
-      }
-      // Replace each 'Y' in the current string with the Y rule
-      else if (current_string[i] == 'Y') {
-        for (k = 0; ruleY[k] != '\0'; k++) {
-          next_string[j++] = ruleY[k];
-        }
-      } else {
-        next_string[j++] = current_string[i];
-      }
     }
-    next_string[j] = '\0';
+    current_string[j] = '\0';
 
-    // Copy the next string to the current string
-    strcpy(current_string, next_string);
-  }
-
-  // Remove all occurrences of X and Y from the final string
-  for (i = 0, j = 0; current_string[i] != '\0'; i++) {
-    if (current_string[i] == 'X' || current_string[i] == 'Y') {
-      continue;
-    } else {
-      current_string[j++] = current_string[i];
+    // Write the final string to a file
+    FILE* fp = fopen("fractal.txt", "w");
+    if (fp == NULL) {
+        printf("Failed to open the file.\n");
+        return 1;
     }
-  current_string[j] = '\0';
-  strcpy(current_string, next_string);
-  }
+    fprintf(fp, "fractal final:\n");
+    fprintf(fp, "%s\n", current_string);
+    fclose(fp);
 
-  // Write the final string to a file
-  FILE* fp = fopen("fractal.txt", "w");
-  fprintf(fp, "%s", current_string);
-  fclose(fp);
-
-  return 0;
+    return 0;
 }
--- TP  -  Matemática Discreta/LcFractal/fractal.c
+++ TP  -  Matemática Discreta/LcFractal/fractal.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <SDL2/SDL.h>
+#include <string.h>
 
 #define MAX_STRING_LENGTH 1000000
 
@@ -10,29 +9,29 @@ int main()
     char axiom[] = "X";
     char ruleX[] = "YF+XF+Y";
     char ruleY[] = "XF-YF-X";
-  
+
     char current_string[MAX_STRING_LENGTH] = {0};
     char next_string[MAX_STRING_LENGTH] = {0};
     int i, j, k, num_iterations;
-  
+
     // Set the current string to the axiom
     strcpy(current_string, axiom);
-  
+
     // Open the file for writing
     FILE* fp = fopen("fractal.txt", "w");
     if (fp == NULL) {
         printf("Failed to open the file.\n");
         return 1;
     }
-  
+
     // Iterate the specified number of times
     for (num_iterations = 0; num_iterations < 4; num_iterations++) {
         // Write the iteration number to the file
         fprintf(fp, "Iteração %d:\n", num_iterations);
-  
+
         // Write the current string to the file
         fprintf(fp, "%s\n", current_string);
-  
+
         // Replace each 'X' in the current string with the X rule
         for (i = 0, j = 0; current_string[i] != '\0'; i++) {
             if (current_string[i] == 'X') {
@@ -50,17 +49,26 @@ int main()
             }
         }
         next_string[j] = '\0';
-  
+
         // Copy the next string to the current string
         strcpy(current_string, next_string);
     }
-  
+
+    // Remove the 'X' and 'Y' symbols from the final string
+    char final_string[MAX_STRING_LENGTH] = {0};
+    for (i = 0, j = 0; current_string[i] != '\0'; i++) {
+        if (current_string[i] != 'X' && current_string[i] != 'Y') {
+            final_string[j++] = current_string[i];
+        }
+    }
+    final_string[j] = '\0';
+
     // Write the final iteration
-    fprintf(fp, "Iteração %d:\n", num_iterations);
-    fprintf(fp, "%s\n", current_string);
-  
+    fprintf(fp, "fractal final:\n");
+    fprintf(fp, "%s\n", final_string);
+
     // Close the file
     fclose(fp);
-  
+
     return 0;
 }
